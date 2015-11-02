16c725e8df4270e3e3736697fa24936c0f0970c1
Ben Weitzman <benweitzman@gmail.com>
2015-11-02 15:36:05 -0500

    ToJSON and quickcheck test

--- src/Data/Json/Schema.hs
+++ src/Data/Json/Schema.hs
@@ -4,14 +4,20 @@
 module Data.Json.Schema where
 
 import Data.Aeson.Types (Parser)
-import Data.Aeson hiding (Object, Number)
+import Data.Aeson hiding (Object, Number, Bool, String, Array)
 import qualified Data.Aeson as A
+
+import Data.ByteString (ByteString)
 
 import Data.Hashable
 
 import qualified Data.HashMap.Strict as HM
 
+import Data.Monoid
+
 import Data.Proxy
+
+import Data.Scientific
 
 import Data.Text hiding (stripSuffix)
 
@@ -20,16 +26,17 @@ import Data.Vector hiding (sequence)
 class ToSchema a where
     toSchema :: Proxy a -> Schema
 
-data Field = Plain Schema
-           | Optional Schema
-           | Exact Value Schema deriving (Eq, Show)
+data FieldQualifier = Plain
+                    | Optional
+                    | Exact deriving (Eq, Show)
 
-data Schema = Boolean
-            | Number
-            | Text
-            | List (Vector Schema)
+data Field = Field FieldQualifier Schema deriving (Eq, Show)
+
+data Schema = Bool Bool
+            | Number Scientific
+            | String Text
+            | Array (Vector Schema)
             | Object (HM.HashMap Text Field) deriving (Eq, Show)
-
 
 stripSuffix :: Text -> Text
 stripSuffix s
@@ -37,27 +44,39 @@ stripSuffix s
     | "?" `isSuffixOf` s = dropEnd 1 s
     | otherwise = s
 
+addSuffix :: Text -> Field -> Text
+addSuffix k (Field Plain _) = k
+addSuffix k (Field Optional _) = k <> "?"
+addSuffix k (Field Exact _) = k <> "="
+
 parseField :: Text -> Value -> Parser Field
 parseField key value
-    | "=" `isSuffixOf` key = Exact value <$> parseJSON value
-    | "?" `isSuffixOf` key = Optional <$> parseJSON value
-    | otherwise = Plain <$> parseJSON value
+    | "=" `isSuffixOf` key = Field Exact <$> parseJSON value
+    | "?" `isSuffixOf` key = Field Optional <$> parseJSON value
+    | otherwise = Field Plain <$> parseJSON value
 
 mapKeys :: (Hashable k1, Hashable k2, Eq k2) => (k1 -> k2) -> HM.HashMap k1 v -> HM.HashMap k2 v
 mapKeys f = HM.foldlWithKey' (\m' k v -> HM.insert (f k) v m') HM.empty
 
+mapKeysWithValues :: (Hashable k1, Hashable k2, Eq k2) => (k1 -> v -> k2) -> HM.HashMap k1 v -> HM.HashMap k2 v
+mapKeysWithValues f = HM.foldlWithKey' (\m' k v -> HM.insert (f k v) v m') HM.empty
+
 instance FromJSON Schema where
-    parseJSON (Bool _) = return Boolean
+    parseJSON (A.Bool x) = return (Bool x)
 
-    parseJSON (A.Number _) = return Number
+    parseJSON (A.Number s) = return (Number s)
 
-    parseJSON (String _) = return Text
+    parseJSON (A.String t) = return (String t)
 
-    parseJSON (Array a) = List <$> sequence (parseJSON <$> a)
+    parseJSON (A.Array a) = Array <$> sequence (parseJSON <$> a)
 
     parseJSON (A.Object o) = Object <$> sequence (mapKeys stripSuffix $ HM.mapWithKey parseField o)
 
     parseJSON Null = mempty
 
-
-
+instance ToJSON Schema where
+    toJSON (Bool x) = A.Bool x
+    toJSON (Number s) = A.Number s
+    toJSON (String t) = A.String t
+    toJSON (Array v) = A.Array (toJSON <$> v)
+    toJSON (Object o) = A.Object (HM.map (\(Field fq s) -> toJSON s) (mapKeysWithValues addSuffix o))
--- test/ParseSpec.hs
+++ test/ParseSpec.hs
@@ -1,19 +1,68 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module ParseSpec where
 
 import Data.Json.Schema
 
-import Data.Aeson hiding (Object, Number)
+import Control.Monad
+
+import Data.Aeson hiding (Object, Number, Bool, Array, String)
+
+import Data.Char (chr)
+
+import qualified Data.HashMap.Strict as HM
+
+import Data.Scientific
+
+import Data.Text
+
+import Data.Vector
+
+import System.IO.Unsafe
 
 import Test.Hspec
 import Test.Hspec.QuickCheck
+
+import Test.QuickCheck
 
 spec :: Spec
 spec =
     describe "parser" $ do
         it "should parse correctly" $ do
-            decode "{\"maybeNum?\":50}" `shouldBe` Just (Object [("maybeNum", Optional Number)])
-            decode "{\"exactNum=\":50}" `shouldBe` Just (Object [("exactNum", Exact (toJSON (50 :: Integer)) Number)])
-            decode "{\"num\":50}" `shouldBe` Just (Object [("num", Plain Number)])
+            decode "{\"maybeNum?\":50}" `shouldBe` Just (Object [("maybeNum", Field Optional (Number 50))])
+            decode "{\"exactNum=\":50}" `shouldBe` Just (Object [("exactNum", Field Exact (Number 50))])
+            decode "{\"num\":50}" `shouldBe` Just (Object [("num", Field Plain (Number 50))])
+        prop "should make the round trip" $
+            \(s::Schema) -> (decode $ encode s) == Just s
+
+instance Arbitrary Scientific where
+  arbitrary = scientific <$> arbitrary <*> arbitrary
+
+instance Arbitrary Text where
+  arbitrary = dropWhileEnd (liftM2 (||) (=='=') (=='?')) . pack <$> listOf1 validChars
+    where validChars = chr <$> choose (55, 126)
+
+instance Arbitrary Schema where
+  arbitrary = sized sizedSchema
+
+sizedSchema :: Int -> Gen Schema
+sizedSchema 0 = oneof [ Bool <$> arbitrary
+                      , Number <$> arbitrary
+                      , String <$> arbitrary
+                      , return $ Array []
+                      , return $ Object []
+                      ]
+sizedSchema n = oneof [ Bool <$> arbitrary
+                      , Number <$> arbitrary
+                      , String <$> arbitrary
+                      , Array . fromList <$> vectorOf (n - 1) (sizedSchema (n `div` 5))
+                      , Object . HM.fromList <$> vectorOf (n - 1) ((,) <$> arbitrary <*> (sizedField (n `div` 5)))
+                      ]
+
+sizedField :: Int -> Gen Field
+sizedField n = Field <$> arbitrary <*> sizedSchema n
+
+instance Arbitrary FieldQualifier where
+  arbitrary = oneof [return Plain, return Optional, return Exact]
